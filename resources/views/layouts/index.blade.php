115337fe12bdf5c7cf9f9d2367ff7d434044ac50
Alim Suma <sumaalim@gmail.com>
2023-10-12 22:00:48 +0800

    updated layout index

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="dark">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <base href="{{ \URL::to('/') }}">
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('faviconn.ico') }}">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+
+</head>
+
+<body>
+    <main class="mx-auto lg:max-w-7xl md:px-8 flex flex-col min-h-screen bg-white dark:bg-gray-900">
+        {{-- @include('components.header') --}}
+        @include('components.Navbar')
+        @yield('content')
+        @include('components.footer')
+    </main>
+</body>
+
+</html>
